095ec64294ad198328a02bb1439bf2c2cb05a31a
Luis Eduardo Setin de Carvalho Cruz <luis.edeploy@stelo.com.br>
2019-12-18 16:58:38 -0300

    capitulo 03. 53. Outputting a List of Recipes with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is a simply test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    new Recipe('A Test Recipe', 'This is a simply test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('A Test Recipe 02', 'Lorem ipsum dolor sit amet', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
   ];
 
   constructor() { }
